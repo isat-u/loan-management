ea5d46ee995d0c3b0361e6aa020b57d11210cedb
Mark <markjungersaniva@gmail.com>
2022-10-09 20:50:54 +0800

    Updated create migration, added users module

--- create_migration.sh
+++ create_migration.sh
@@ -4,3 +4,5 @@ mkdir -p accounts/migrations && touch accounts/migrations/__init__.py
 mkdir -p datetimedim/migrations && touch datetimedim/migrations/__init__.py
 mkdir -p profiles/migrations && touch profiles/migrations/__init__.py
 mkdir -p locations/migrations && touch locations/migrations/__init__.py
+mkdir -p payment_histories/migrations && touch payment_histories/migrations/__init__.py
+mkdir -p users/migrations && touch users/migrations/__init__.py
